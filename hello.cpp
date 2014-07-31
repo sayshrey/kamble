44024b73afc06e457be399d8124ff5b9d610d503
sayshrey <sayali.mahamine@cumminscollege.in>
2014-07-31 14:34:54 +0530

    Update hello.cpp
    I have edited my first github file...

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <iostream.h>
+
 void main()
 {
   cout<<"HELLO FRIENDS!!! WELCOME!!!";
+}
+double square(double num)
+{
+  double outnum;
+  outnum=num*num;
 }
